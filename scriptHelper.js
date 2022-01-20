cf0483adcbb02b0f6e9be8f2111601cb31046155
Emma Macleod <emmamacleod@live.com>
2022-01-20 15:21:37 -0500

    2nd Commit

--- scriptHelper.js
+++ scriptHelper.js
@@ -3,54 +3,81 @@ require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
    window.addEventListener("load", function() {
-       document.getElementById("missionTarget").innerHTML
-   });
-   // Here is the HTML formatting for our mission target div.
-   /*
+       document.getElementById("missionTarget").innerHTML = `
+   
                 <h2>Mission Destination</h2>
                 <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
-                    <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
+                    <li>Name: ${json[index].name}</li>
+                    <li>Diameter:${json[index].diameter}</li>
+                    <li>Star: ${json[index].star}</li>
+                    <li>Distance from Earth:${json[index].distance} </li>
+                    <li>Number of Moons:${json[index].moons} </li>
                 </ol>
-                <img src="">
-   */
+                <img src="${json[index].image}"> 
+            `;
+});
 }
 
 function validateInput(testInput) {
     window.addEventListener("load", function() { 
-        let form = document.querySelector("testForm");
+        let testInput = document.querySelector("testForm");
         form.addEventListener("submit", function (event) {
-            let pilotNameInput = document.querySelector("input[name=pilotName]");
+            /*let pilotNameInput = document.querySelector("input[name=pilotName]");
             let copilotNameInput = document.querySelector("input[name=copilotName]");
             let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
             let cargoMassInput = document.querySelector("input[name=cargoMass]");
-        if (pilotNameInput.value === "" || copilotNameInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value === "") {
-            alert("All fields are required!");
-            event.stopPropagation();
-        } else if (pilotNameInput.value === !"" || copilotNameInput.value === !"") {
+            */
+        if (testInput.value === "") {
             return "Empty";
+        } else if (isNaN() == true) {
+            return "Not a Number";
+        } else 
+            return "Is a Number"
+        /*} else if (pilotNameInput.value === !"" || copilotNameInput.value === !"") {
+            return "Empty";
+            event.preventDefault();
         } else if (isNaN(fuelLevelInput.value) == true || isNaN(cargoMassInput.value) == true) {
             return "Not a Number";
+            event.preventDefault();
         } else (isNaN(fuelLevelInput.value) == false || isNaN(cargoMassInput.value) == false) {
             return "Is a Number";
-        }
-      }
+            event.preventDefault();
+        }*/
+      });
      });
-   });
+   };
    
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
    let button = document.getElementById("formSubmit");
    button.addEventListener("click", function () {
-       let updatePilotName = document.getElementById("pilotStatus");
-       let updateCopilotName = document.getElementById("copilotStatus");
-       let updateFuelLevel = document.getElementById("fuelStatus");
-       let updateCargoMass = document.getElementById("cargoStatus");
+      // variables for calling <form> input
+       let pilotName = document.querySelector("input[name=pilotName]");
+       let copilotName = document.querySelector("input[name=copilotName]");
+       let fuelLevel = document.querySelector("input[name=fuelLevel]");
+       let cargoMass = document.querySelector("input[name=cargoMass]");
+      // variables for calling elements
+       let faultyItems = document.getElementById('faultyItems');
+       let launchStatus = document.getElementById('launchStatus');
+       let fuelStatus = document.getElementById('fuelStatus');
+       let pilotStatus = document.getElementById('pilotStatus');
+       let copilotStatus = document.getElementById('copilotStatus');
 
-    updatePilotName.innerHTML = `Pilot ${document.querySelector("input[name=pilotName]")} is ready for launch!`;
+       // make sure all inputs have a string value 
+if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
+    alert("All field are required!");
+    event.preventDefault();
+}
+    //make sure pilot and copilot names are input as strings 
+if (isNaN(pilotName.value) || isNan(copilotName.value)) {
+    pilotStatus.innerHTML = 'Pilot ${pilotName.value} is ready';
+    copilotStatus.innerHTML = 'Co-pilot ${copilotName.value} is ready';
+}
+else {
+    alert ()
+}
+}
+    /*updatePilotName.innerHTML = `Pilot ${document.querySelector("input[name=pilotName]")} is ready for launch!`;
     updateCopilotName.innerHTML =  `Copilot ${document.querySelector("input[name=copilotName")} is ready for launch!`;
 
 let newHeader = document.getElementById("launchStatus");
@@ -66,7 +93,7 @@ if (document.querySelector("input[name=fuelLevel]") < 10000) {
 } else {
     document.getElementById("launchStatus").style.color = "green";
     newHeader.innerHTML = `Shuttle is ready for Launch!`;
-}
+}*/
  });
     }
 
